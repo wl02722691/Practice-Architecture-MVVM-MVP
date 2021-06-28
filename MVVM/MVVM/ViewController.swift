d55d3362f8686492635667c16730e69d602869ed
Alice <wl02722691@gmail.com.tw>
2021-06-28 14:50:30 +0800

    Add MVVM

---
+++ MVVM/MVVM/ViewController.swift
@@ -0,0 +1,101 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by 張書涵 on 2021/6/28.
+//
+
+import UIKit
+
+// Observable
+
+class Observable<T> {
+    var value : T? {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    init(_ value: T?) {
+        self.value = value
+    }
+    
+    private var listener: ((T?) -> Void)?
+    
+    func bind(_ listener: @escaping (T?) -> Void) {
+        listener(value)
+        self.listener = listener
+    }
+}
+
+// Model
+struct User: Codable {
+    let name: String
+}
+
+// ViewModels
+
+struct UserListViewModel {
+    var users: Observable<[UserTableViewCellModel]> = Observable([])
+}
+
+struct UserTableViewCellModel {
+    let name: String
+}
+
+// Controller
+class ViewController: UIViewController {
+
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private let viewModel = UserListViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        tableView.dataSource = self
+        
+        viewModel.users.bind { [weak self] _ in
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
+        }
+        fetchData()
+    }
+
+    func fetchData() {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") else { return }
+        let task = URLSession.shared.dataTask(with: url) { (data, _, _) in
+            guard let data = data else { return }
+            do {
+                let userModels = try JSONDecoder().decode([User].self, from: data)
+                self.viewModel.users.value = userModels.compactMap({
+                    UserTableViewCellModel(name: $0.name)
+                })
+            } catch {
+                
+            }
+        }
+        
+        task.resume()
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.users.value?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = viewModel.users.value?[indexPath.row].name
+        return cell
+    }
+    
+    
+}
